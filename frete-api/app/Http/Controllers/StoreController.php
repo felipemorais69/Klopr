0ee8240393de139378a9d2ed581e210ab9628670
aldemarf <aldemar.poli@gmail.com>
2019-11-05 07:40:34 -0300

    Controller das lojas -- Métodos  listStores + detailStore

---
+++ frete-api/app/Http/Controllers/StoreController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+class StoreController extends Controller
+{
+
+    public function listStores(Request $request)
+    {
+        $requestUrl = $request->fullUrl();
+        $token = $request->bearerToken(); // Formato: 'Bearer {token}'
+
+        $cHandle = curl_init();
+        $domain = 'https://sandbox.melhorenvio.com.br'; // ALTERAR!!!
+        $endpoint = '/api/v2/me/companies';
+        $header = array(
+            'Accept: application/json',
+            'Content-type: application/json',
+            'Authorization: ' . $token);
+        //$query = $request->query();
+
+        curl_setopt_array($cHandle,[
+            CURLOPT_URL => $domain . $endpoint/* . $query*/,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_HTTPHEADER => $header,
+            //CURLOPT_HTTPAUTH => CURLAUTH_BASIC,
+            //CURLOPT_FOLLOWLOCATION => false,
+            //CURLOPT_TIMEOUT => 5,
+        ]);
+
+        $output = trim(curl_exec($cHandle));
+        curl_close($cHandle);
+
+        return response($output, 200);
+    }
+
+    public function detailStore(Request $request, $id='')
+    {
+        if (!is_int($id)) {
+            return response('ID da loja não informado', 400);
+        }
+
+        $requestUrl = $request->fullUrl();
+        $token = $request->bearerToken(); // Formato: 'Bearer {token}'
+
+        $curl = curl_init();
+        $domain = 'https://sandbox.melhorenvio.com.br'; // ALTERAR!!!
+        $endpoint = '/api/v2/me/companies/';
+        $header = array(
+            'Accept: application/json',
+            'Content-type: application/json',
+            'Authorization: ' . $token);
+        $query = '?id_loja=' . $id;
+
+        curl_setopt_array($curl,[
+            CURLOPT_URL => $domain . $endpoint . $query,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_HTTPHEADER => $header,
+            //CURLOPT_HTTPAUTH => CURLAUTH_BASIC,
+            //CURLOPT_FOLLOWLOCATION => false,
+            //CURLOPT_TIMEOUT => 5,
+        ]);
+
+        $output = trim(curl_exec($curl));
+        curl_close($curl);
+
+        return response($output, 200);
+    }
+}
