9ec3f3310a9973495bc164a3c94b8e835931acd2
aldemarf <aldemar.poli@gmail.com>
2019-11-10 17:52:00 -0300

    Unnecessary method deleted

--- frete-api/app/Http/Controllers/CheckoutController.php
+++ frete-api/app/Http/Controllers/CheckoutController.php
@@ -8,25 +8,6 @@ use Illuminate\Http\Request;
 class CheckoutController extends Controller
 {
 
-    public function removeShipping (Request $request, $id)
-    {
-        $requestUrl = $request->fullUrl();
-        $token = $request->bearerToken(); // Formato: 'Bearer {token}'
-        $endpoint = '/api/v2/me/cart/{id}';
-        $header = array(
-            'Accept: application/json',
-            'Content-type: application/json',
-            'Authorization: ' . $token);
-        $query = '?id=' . $id;
-
-        $response = $this->GetDelRequestCurl(self::domainME, $endpoint, $query, $header);
-        $output = $response['output'];
-        $resultCode = $response['resultCode'];
-
-        return response($output, $resultCode);
-    }
-
-
     public function buyShipping(Request $request)
     {
         /*
