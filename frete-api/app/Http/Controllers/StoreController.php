285524bb46de41577fd6819a671cf8529b458f71
aldemarf <aldemar.poli@gmail.com>
2019-11-10 14:55:21 -0300

    New method: registerStore

--- frete-api/app/Http/Controllers/StoreController.php
+++ frete-api/app/Http/Controllers/StoreController.php
@@ -17,9 +17,8 @@ class StoreController extends Controller
             'Accept: application/json',
             'Content-type: application/json',
             'Authorization: Bearer ' . $token);
-        $query='';
 
-        $response = $this->GetDelRequestCurl(self::domainME, $endpoint, $query, $header);
+        $response = $this->GetDelRequestCurl(self::domainME, $endpoint, null, $header);
         $output = $response['output'];
         $resultCode = $response['resultCode'];
 
@@ -38,6 +37,32 @@ class StoreController extends Controller
         $query = $id;
 
         $response = $this->GetDelRequestCurl(self::domainME, $endpoint, $query, $header);
+        $output = $response['output'];
+        $resultCode = $response['resultCode'];
+
+        return response($output, $resultCode);
+    }
+
+    public function registerStore(Request $request)
+    {
+        $requestUrl = $request->fullUrl();
+        $token = $request->bearerToken();
+        $endpoint = '/api/v2/me/balance';
+        $header = array(
+            'Accept: application/json',
+            'Content-type: application/json',
+            'Authorization: Bearer ' . $token);
+        $fields = array(
+            'name' => $request->request->get('name'), //Obrigatório -- Nome de identificação no sistema
+            'email' => $request->request->get('email'), //Obrigatório -- email da loja
+            'picture' => $request->request->get('picture'), //Opcional -- Imagem
+            'description' => $request->request->get('description'), //Opcional -- Descrição da loja
+            'company_name' => $request->request->get('company_name'), //Obrigatório -- Nome da loja
+            'document' => $request->request->get('document'), //Obrigatório -- CNPJ da loja
+            'state_register' => $request->request->get('state_register'), //Opcional
+        );
+
+        $response = $this->PostRequestCurl(self::domainSandboxME, $endpoint, $header, $fields, 'URL');
         $output = $response['output'];
         $resultCode = $response['resultCode'];
 
