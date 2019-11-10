5d67acc271317f5ea32d1a465ad6638a664042f6
aldemarf <aldemar.poli@gmail.com>
2019-11-10 14:56:09 -0300

    New method: addCredit | new comments

--- frete-api/app/Http/Controllers/UserController.php
+++ frete-api/app/Http/Controllers/UserController.php
@@ -17,28 +17,50 @@ class UserController extends Controller
             'Accept: application/json',
             'Content-type: application/json',
             'Authorization: Bearer ' . $token);
-
         $fields = array(
-            'firstname' => $request->request->get('firstname'), //Obrigatório
-            'lastname' => $request->request->get('lastname'), //Obrigatório
-            'document' => $request->request->get('document'), //Obrigatório
-            'birthdate' => $request->request->get('birthdate'), //Obrigatório
-            'email' => $request->request->get('email'), //Obrigatório
-            'password' => $request->request->get('password'), //Obrigatório
-            'phone_mobile' => $request->request->get('mobile-phone'), //Obrigatório
-            'phone_fixed' => $request->request->get('fixed-phone'), //Opcional
-            'company' => $request->request->get('company'), //Opcional
-            'coupon' => $request->request->get('coupon'), //Opcional
-            'terms' => $request->request->get('terms'), //Obrigatório
-            'address[label]' => $request->request->get('address-label'), //Opcional
-            'address[postal_code]' => $request->request->get('postal-code'), //Obrigatório
-            'address[address]' => $request->request->get('address'), //Obrigatório
-            'address[number]' => $request->request->get('address-number'), //Obrigatório
-            'address[complement]' => $request->request->get('address-complement'), //Opcional
-            'address[district]' => $request->request->get('district'),  //Opcional
-            'address[city]' => $request->request->get('city'), //Obrigatório
-            'address[state_abbr]' => $request->request->get('state-abbr'), //Obrigatório
-            'address[country]' => $request->request->get('country'), //Opcional
+            'firstname' => $request->request->get('firstname'), //Obrigatório -- Primeiro nome
+            'lastname' => $request->request->get('lastname'), //Obrigatório -- Sobrenome
+            'document' => $request->request->get('document'), //Obrigatório -- CPF
+            'birthdate' => $request->request->get('birthdate'), //Obrigatório -- Data de nascimento
+            'email' => $request->request->get('email'), //Obrigatório -- email
+            'password' => $request->request->get('password'), //Obrigatório -- senha
+            'phone_mobile' => $request->request->get('phone_mobile'), //Obrigatório -- Celular
+            'phone_fixed' => $request->request->get('phone_fixed'), //Opcional -- Telefone fixo
+            'company' => $request->request->get('company'), //Opcional -- Nome da loja
+            'coupon' => $request->request->get('coupon'), //Opcional -- CUPOM
+            'terms' => $request->request->get('terms'), //Obrigatório -- Aceitação de termos do uso
+            'address[label]' => $request->request->get('address[label]'), //Opcional -- Identificação do endereço
+            'address[postal_code]' => $request->request->get('address[postal_code]'), //Obrigatório -- CEP
+            'address[address]' => $request->request->get('address[address]'), //Obrigatório -- Rua/Logradouro
+            'address[number]' => $request->request->get('address[number]'), //Obrigatório -- Número do imóvel
+            'address[complement]' => $request->request->get('address[complement]'), //Opcional -- Complemento do endereço
+            'address[district]' => $request->request->get('address[district]'),  //Opcional -- Bairro
+            'address[city]' => $request->request->get('address[city]'), //Obrigatório -- Cidade
+            'address[state_abbr]' => $request->request->get('address[state_abbr]'), //Obrigatório -- Sigla do Estado
+            'address[country]' => $request->request->get('address[country]'), //Opcional -- País
+        );
+
+        $response = $this->PostRequestCurl(self::domainSandboxME, $endpoint, $header, $fields, 'JSON');
+        $output = $response['output'];
+        $resultCode = $response['resultCode'];
+
+        return response($output, $resultCode);
+    }
+
+
+    public function addCredit(Request $request)
+    {
+        $requestUrl = $request->fullUrl();
+        $token = $request->bearerToken();
+        $endpoint = '/api/v2/me/balance';
+        $header = array(
+            'Accept: application/json',
+            'Content-type: application/json',
+            'Authorization: Bearer ' . $token);
+        $fields = array(
+            'gateway' => $request->request->get('gateway'), //Obrigatório -- moip / mercado-pago
+            'redirect' => $request->request->get('redirect'), //Opcional -- URL de redirecionamento
+            'value' => $request->request->get('value'), //Obrigatório -- Valor monetário
         );
 
         $response = $this->PostRequestCurl(self::domainSandboxME, $endpoint, $header, $fields, 'JSON');
