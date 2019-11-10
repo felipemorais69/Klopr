c3001ebd114f387beb2c15f4274b83ebe1f49bea
aldemarf <aldemar.poli@gmail.com>
2019-11-10 14:54:27 -0300

    New methods: detailItem + listItem

--- frete-api/app/Http/Controllers/CartController.php
+++ frete-api/app/Http/Controllers/CartController.php
@@ -11,26 +11,57 @@ class CartController extends Controller
 
     public function delItem(Request $request)
     {
-
-        /*
-        HEADERS
-        Accept: application/json
-        Content-Type: application/json
-        Authorization : Bearer {{token}}
-        */
-
         $requestUrl = $request->fullUrl();
-        $token = $request->bearerToken(); // Formato: 'Bearer {token}'
+        $token = $request->bearerToken();
         $endpoint = '/api/v2/me/cart/{id}';
         $header = array(
             'Accept: application/json',
             'Content-type: application/json',
             'Authorization: Bearer ' . $token);
 
-        $response = $this->GetDelRequestCurl(self::domainME, $endpoint, $header);
+        $response = $this->GetDelRequestCurl(self::domainME, $endpoint, null, $header);
         $output = $response['output'];
         $resultCode = $response['resultCode'];
 
         return response($output, $resultCode);
     }
+
+
+    public function listItems(Request $request)
+    {
+        $requestUrl = $request->fullUrl();
+        $token = $request->bearerToken();
+        $endpoint = '/api/v2/me/cart';
+        $header = array(
+            'Accept: application/json',
+            'Content-type: application/json',
+            'Authorization: Bearer ' . $token);
+
+        $response = $this->GetDelRequestCurl(self::domainME, $endpoint, null, $header);
+        $output = $response['output'];
+        $resultCode = $response['resultCode'];
+
+        return response($output, $resultCode);
+    }
+
+
+    public function detailItem(Request $request, $id)
+    {
+        $requestUrl = $request->fullUrl();
+        $token = $request->bearerToken();
+        $endpoint = '/api/v2/me/cart';
+        $header = array(
+            'Accept: application/json',
+            'Content-type: application/json',
+            'Authorization: Bearer ' . $token);
+        $query = '/' . $id;
+
+        $response = $this->GetDelRequestCurl(self::domainSandboxME, $endpoint, $query, $header);
+        $output = $response['output'];
+        $resultCode = $response['resultCode'];
+
+        return response($output, $resultCode);
+    }
+
+
 }
