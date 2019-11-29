8c08495d3503e3f4e25425653d04a65e5b3a64b0
aldemarf <aldemar.poli@gmail.com>
2019-11-29 20:27:19 -0300

    Corrected function: addCredit

--- frete-api/app/Http/Controllers/UserController.php
+++ frete-api/app/Http/Controllers/UserController.php
@@ -84,7 +84,7 @@ class UserController extends Controller
         $endpoint = '/api/v2/me/balance';
         $header = array(
             'Accept: application/json',
-            'Content-type: application/json',
+            'Content-type: application/x-www-form-urlencoded',
             'Authorization: Bearer ' . $token);
         $fields = array(
             'gateway' => $request->input('gateway'), //Obrigatório -- moip / mercado-pago
@@ -92,7 +92,7 @@ class UserController extends Controller
             'value' => $request->input('value'), //Obrigatório -- Valor monetário
         );
 
-        $response = $this->PostRequestCurl(self::domainSandboxME, $endpoint, $header, $fields, 'JSON');
+        $response = $this->PostRequestCurl(self::domainSandboxME, $endpoint, $header, $fields, 'URL');
         $output = $response['output'];
         $resultCode = $response['resultCode'];
 
